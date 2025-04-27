4ad1114572924187d90affe6efdc627e78f2c493
dfnzhc <z69500jqk@qq.com>
2025-04-27 18:45:20 +0800

    feat(memory): add PlainAllocator using mimalloc and extend IAllocator interface

--- Source/Bee/Memory/Allocators/IAllocator.hpp
+++ Source/Bee/Memory/Allocators/IAllocator.hpp
@@ -8,6 +8,7 @@
 #pragma once
 
 #include "Core/Defines.hpp"
+#include "Core/Portability.hpp"
 
 namespace bee {
 
@@ -16,9 +17,40 @@ class BEE_API IAllocator
 public:
     virtual ~IAllocator() = default;
 
-    virtual void* allocate(Size size, Size alignment) = 0;
-    virtual void* allocate(Size size, Size alignment, const char* file, const char* line, const char* label = nullptr) = 0;
-    
-    virtual void deallocate(void* ptr) = 0; 
+    /**
+     * @brief Allocates a block of memory with the specified size and alignment.
+     *
+     * @param size The desired size of the memory block in bytes.
+     * @param alignment The desired alignment of the memory block in bytes. Must be a power of two.
+     * @return A pointer to the allocated memory block on success, or nullptr on failure (e.g., out of memory, invalid parameters).
+     */
+    BEE_NODISCARD virtual void* allocate(Size size, Size alignment = 0) = 0;
+
+    /**
+     * @brief Deallocates a previously allocated block of memory.
+     *
+     * @param ptr A pointer to the memory block to deallocate.
+     */
+    virtual void deallocate(void* ptr) = 0;
+
+    /**
+     * @brief Return the available bytes in a memory block.
+     *
+     * @param ptr Pointer to allocated memory.
+     * @return The available bytes in the memory block, or 0 if p was nullptr or not allocated from the allocator.
+     */
+    BEE_NODISCARD virtual Size usableSize(void* ptr) = 0;
+
+    /**
+     * @brief Check weather the memory block allocated from this allocator.
+     *
+     * @param ptr A pointer to the memory block.
+     */
+    BEE_NODISCARD virtual bool owns(void* ptr) const = 0;
+
+    /**
+     * @return The name of the allocator.
+     */
+    BEE_NODISCARD virtual const char* name() const = 0;
 };
 } // namespace bee
