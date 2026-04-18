b20dc25493ac2a22672c980ebfe53437964fb061
dfnzhc <z69500jqk@qq.com>
2026-04-18 14:10:07 +0800

    feat(task): 添加 Task V2 聚合头文件

---
+++ Bee/Task/Task.hpp
@@ -0,0 +1,16 @@
+/**
+ * @File Task.hpp
+ * @Author dfnzhc (https://github.com/dfnzhc)
+ * @Date 2026/4/18
+ * @Brief This file is part of Bee.
+ */
+
+#pragma once
+
+// Task V2 聚合头文件
+#include "Task/Core/TaskState.hpp"
+#include "Task/Core/Task.hpp"
+#include "Task/Core/Scheduler.hpp"
+#include "Task/Core/WhenAll.hpp"
+#include "Task/Core/WhenAny.hpp"
+#include "Task/Core/AsyncScope.hpp"
